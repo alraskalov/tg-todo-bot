d98f8fc4c64a847105f400c350429514952c48f1
Alexander Dolzhenko <raskalovalexander@gmail.com>
2022-07-25 17:24:08 +0300

    feat: mongoose, created model files

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 
 const express = require('express');
 const { Telegraf } = require('telegraf');
+const mongoose = require('mongoose');
 
-const { PORT = 3000, BOT_TOKEN } = process.env;
+const { PORT = 3000, BOT_TOKEN, DB_URL } = process.env;
 
 if (BOT_TOKEN === undefined) {
   throw new TypeError('BOT_TOKEN must be provided!');
 }
+
 const app = express();
+
+mongoose.connect(DB_URL);
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -18,6 +22,7 @@ bot.start((ctx) => ctx.reply('Добро пожаловать в ToDo'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('emoji', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.help((ctx) => ctx.setChatMenuButton(['123', '321']));
 
 app.listen(PORT);
 
